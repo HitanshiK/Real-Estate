4cf7b832de161e9b09ae9afa06827b28fef82727
HitanshiK <112873937+HitanshiK@users.noreply.github.com>
2024-06-20 19:37:17 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,11 +31,11 @@ app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use("/api/listing",listingRouter);
 
-app.use(express.static(path.join(_dirname,'/client/dist')));
+/*app.use(express.static(path.join(_dirname,'/client/dist')));
 
 app.get('*',(req,res)=>{
     res.sendFile(path.join(_dirname,'client','dist','index.html'));
-});
+});*/
 
 app.use((err,req,res,next)=>{
    const statusCode = err.statusCode || 500;
